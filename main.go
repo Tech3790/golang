d26e3f89a4f0128d66fad498cc6405f6e72c2d18
Abdullah Alabasi <abdullah@Abdullahs-MBP.attlocal.net>
2020-09-18 17:42:41 -0500

    Added Shuffle function

--- main.go
+++ main.go
@@ -5,5 +5,9 @@ import (
 )
 
 func main() {
+	a := []int{1, 2, 3, 4}
+	b := []int{1, 2, 3, 4}
 	fmt.Println(Reverse("!oG ,olleH"))
+	RunningSum(b)
+	fmt.Println(Shuffle(a, 2))
 }
--- reverse.go
+++ reverse.go
@@ -8,3 +8,20 @@ func Reverse(word string) string {
 	}
 	return string(foo)
 }
+
+// RunningSum is a function to sum all elements in array
+func RunningSum(nums []int) []int {
+	for i := 1; i < len(nums); i++ {
+		nums[i] = nums[i] + nums[i-1]
+	}
+	return nums
+}
+
+// Shuffle is a function to shuffle an array
+func Shuffle(nums []int, n int) []int {
+	res := make([]int, len(nums))
+	for i := range nums[:n] {
+		res[i*2], res[i*2+1] = nums[i], nums[i+n]
+	}
+	return res
+}
