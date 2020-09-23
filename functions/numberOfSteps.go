aca1c1d2c974bfd1ae1c65fe15b79d338c2efa5b
Abdullah Alabasi <abdullah@Abdullahs-MBP.attlocal.net>
2020-09-23 12:25:17 -0500

    Optimized numberOfSteps function

--- functions/numberOfSteps.go
+++ functions/numberOfSteps.go
@@ -3,14 +3,13 @@ package main
 // NumberOfSteps returns the number of steps required to reduce an integer to zero
 func NumberOfSteps(num int) int {
 	steps := 0
-	for i := num; i > 0; {
-		if i%2 == 0 {
-			i = i / 2
-			steps++
+	for num != 0 {
+		if num%2 == 0 {
+			num /= 2
 		} else {
-			i = i - 1
-			steps++
+			num--
 		}
+		steps++
 	}
 	return steps
 }
